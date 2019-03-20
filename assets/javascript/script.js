20a9a7d30a9d3cadde6088a2cafc8492f092eb25
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-03-20 18:10:08 -0500

    added wins losses guesses user guess and basic computer guess

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,8 @@ var userInputs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p
 //Win and Lose
 var wins = 0;
 var losses = 0;
+var guesses = 9;
+//Psychic Function
 document.onkeyup = function(event) {
        var userGuess = event.key;
  console.log (userGuess)
@@ -13,8 +15,21 @@ document.onkeyup = function(event) {
 
               if (userGuess === psychic)
        {wins++}
-              else{losses++};
+              else{losses++ && guesses--};
+console.log(guesses)
 console.log(wins)
 console.log(losses)
+var userText = document.getElementById("choice-text");
+userText.textContent =  "You chose: " + userGuess;
+var psychicText = document.getElementById("psychic-text");
+psychicText.textContent =  "I chose: " + psychic;
+var winsText = document.getElementById("wins-text");
+winsText.textContent = "Times you've won: " + wins; 
+var lossesText = document.getElementById("losses-text");
+lossesText.textContent = "Times I've won: " + losses;
+var guessestext = document.getElementById("guessestext");
+guessestext.textContent = "Guess remaining: " + guesses;
+       }
+if (guesses === 0){alert("Out of guesses! You lose!") && re
 }
 }

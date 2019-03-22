92753766a08c6c45fd3773d0152c409300b7f2e6
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-03-22 14:22:33 -0500

    added parameters taht stopped the user from entering a foreign key or from entering the same letter more than once

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-var userInputs = ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var userInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0
 var losses = 0
 var guesses = 10
@@ -6,51 +6,65 @@ var letters = []
 var computerChoice = []
 //Psychic Function
 
-      var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
-      computerChoice.push(psychic)
-      console.log(psychic[0]);
+var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
+computerChoice.push(psychic)
+console.log(psychic[0]);
 
 
 //UserGuess function
-document.onkeyup = function(event){
-    var userGuess = event.key
-    letters.push(userGuess)
-    console.log(userGuess);  
-
-//logic
-
-if ((userGuess === computerChoice[0]) && (guesses > 0)) {
-      wins++; alert("You win!")
-	guesses = 10;
-	letters.length = 0;
-	computerChoice.length = 0;
-	var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
-	computerChoice.push(psychic[0]);
-      console.log(psychic[0]);
-      }
-else if ((userGuess !== computerChoice[0]) && (guesses > 0)) {
-      guesses--}
-
-else {losses++; alert("You lose!")
-guesses = 10;
-	letters.length = 0;
-	computerChoice.length = 0;
-	var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
-	computerChoice.push(psychic[0]);
-      console.log(psychic[0]);
-
+document.onkeyup = function (event) {
+      var userGuess = event.key
+if(letters.includes(userGuess)){
+      alert("Letter already choosen, pick another")
+      return
 }
-console.log(guesses)
-console.log(wins)
-console.log(losses)
-var userText = document.getElementById("choice-text");
-userText.textContent =  "You chose: " + userGuess;
-var winsText = document.getElementById("wins-text");
-winsText.textContent = "Times you've won: " + wins; 
-var lossesText = document.getElementById("losses-text");
-lossesText.textContent = "Times I've won: " + losses;
-var guessesText = document.getElementById("gtext");
-guessesText.textContent = "Guesses remaining: " + guesses;
-var lettersText = document.getElementById("ltext");
-lettersText.textContent = "Letters you've guessed: " + letters
+
+      //logic
+      if (userInputs.includes(userGuess)) {
+            letters.push(userGuess)
+            console.log(userGuess);
+            if ((userGuess === computerChoice[0]) && (guesses > 0)) {
+                  wins++; alert("You win!")
+                  guesses = 10;
+                  letters.length = 0;
+                  computerChoice.length = 0;
+                  var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
+                  computerChoice.push(psychic[0]);
+                  console.log(psychic[0]);
+
+
+            }
+            else if ((userGuess !== computerChoice[0]) && (guesses > 0)) {
+                  guesses--
+            }
+
+            else {
+                  losses++; alert("You lose!")
+                  guesses = 10;
+                  letters.length = 0;
+                  computerChoice.length = 0;
+                  var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
+                  computerChoice.push(psychic[0]);
+                  console.log(psychic[0]);
+
+            }
+            console.log(guesses)
+            console.log(wins)
+            console.log(losses)
+            var userText = document.getElementById("choice-text");
+            userText.textContent = "You chose: " + userGuess;
+            var winsText = document.getElementById("wins-text");
+            winsText.textContent = "Times you've won: " + wins;
+            var lossesText = document.getElementById("losses-text");
+            lossesText.textContent = "Times I've won: " + losses;
+            var guessesText = document.getElementById("gtext");
+            guessesText.textContent = "Guesses remaining: " + guesses;
+            var lettersText = document.getElementById("ltext");
+            lettersText.textContent = "Letters you've guessed: " + letters
+      }
+      else {
+            alert("Please insert a letter from the alphabet")
+
+      }
+     
 }

edb4af193dda7f4390a53e5193a1b7a0b52e8ecf
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-03-22 10:58:15 -0500

    complete overhaul on game logic after review examples online, probably too many changes for one commit game logic now works except for non a-z characters and capital letters

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,35 +1,55 @@
-//Array of Choices
-var userInputs = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-//Win and Lose
-var wins = 0;
-var losses = 0;
-var guesses = 9;
+var userInputs = ["a", "b", "c", "d", "e", "f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var wins = 0
+var losses = 0
+var guesses = 10
+var letters = []
+var computerChoice = []
 //Psychic Function
-document.onkeyup = function(event) {
-       var userGuess = event.key;
- console.log (userGuess)
-       var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
- console.log(psychic);
-       if ((userGuess === "a" || (userGuess === "b") || (userGuess === "c")) || (userGuess === "d") || (userGuess === "e") || (userGuess === "f")|| 
-       (userGuess === "g")|| (userGuess === "h")||  (userGuess === "i")|| (userGuess === "j")  || (userGuess === "k")|| (userGuess === "l")||(userGuess = "m") || (userGuess === "n")||(userGues = "o")|| (userGuess === "p")|| (userGuess === "q")||(userGuess === "r")||(userGuess === "s")|| (userGuess === "t")|| (userGuess === "u")||(userGuess === "v")||(userGuess === "w")||(userGuess === "x")||(userGuess === "y")||(userGuess ="z")){
+window.onload = function(){
+      var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
+      computerChoice.push(psychic)
+      console.log(psychic[0]);}
 
-              if (userGuess === psychic)
-       {wins++}
-              else{losses++ && guesses--};
+
+//UserGuess function
+document.onkeyup = function(event){
+    var userGuess = event.key
+    letters.push(userGuess)
+    console.log(userGuess);  
+
+//logic
+
+if ((userGuess === computerChoice[0]) && (guesses > 0)) {
+      wins++;
+	guesses = 10;
+	letters.length = 0;
+	computerChoice.length = 0;
+	var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
+	computerChoice.push(psychic[0]);
+      console.log(psychic[0]);
+      }
+else if ((userGuess !== computerChoice[0]) && (guesses > 0)) {
+      guesses--}
+
+else {losses++; 
+guesses = 10;
+	letters.length = 0;
+	computerChoice.length = 0;
+	var psychic = userInputs[Math.floor(Math.random() * userInputs.length)];
+	computerChoice.push(psychic[0]);
+      console.log(psychic[0]);
+}
 console.log(guesses)
 console.log(wins)
 console.log(losses)
 var userText = document.getElementById("choice-text");
 userText.textContent =  "You chose: " + userGuess;
-var psychicText = document.getElementById("psychic-text");
-psychicText.textContent =  "I chose: " + psychic;
 var winsText = document.getElementById("wins-text");
 winsText.textContent = "Times you've won: " + wins; 
 var lossesText = document.getElementById("losses-text");
 lossesText.textContent = "Times I've won: " + losses;
-var guessestext = document.getElementById("guessestext");
-guessestext.textContent = "Guess remaining: " + guesses;
-       }
-if (guesses === 0){alert("Out of guesses! You lose!") && re
-}
+var guessesText = document.getElementById("gtext");
+guessesText.textContent = "Guesses remaining: " + guesses;
+var lettersText = document.getElementById("ltext");
+lettersText.textContent = "Letters you've guessed: " + letters
 }
